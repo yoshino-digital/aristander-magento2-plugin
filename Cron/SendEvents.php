e629012ca3a5d47be9e47975e56dfec76a88216c
Anton Oliinyk <ts2001@gmail.com>
2018-11-23 17:12:23 +0200

    Add ping event

--- Cron/SendEvents.php
+++ Cron/SendEvents.php
@@ -77,6 +77,8 @@ class SendEvents
      */
     public function execute()
     {
+        $this->logger->debug('Starting Aristander.ai event sending task');
+
         try {
             $this->initHttpClient();
         } catch (NotConfiguredException $e) {
@@ -93,78 +95,90 @@ class SendEvents
             $eventCollection->setPageSize($this->maxCount);
         }
 
-        if (0 == $eventCollection->getSize()) {
-            $this->logger->debug("No pending events found, quitting cron job");
-            return;
-        }
+        if (0 != $eventCollection->getSize()) {
+            $this->logger->debug("Found {$eventCollection->getSize()} pending events");
 
-        $this->logger->debug("Found {$eventCollection->getSize()} pending events");
+            for ($pageNo = 1; $pageNo <= $eventCollection->getLastPageNumber(); $pageNo++) {
+                $this->logger->debug("Processing page #{$pageNo} of {$eventCollection->getLastPageNumber()}");
 
-        for ($pageNo = 1; $pageNo <= $eventCollection->getLastPageNumber(); $pageNo++) {
-            $this->logger->debug("Processing page #{$pageNo} of {$eventCollection->getLastPageNumber()}");
+                // Force fetching top pending events
+                $eventCollection->clear();
+                // The loop doesn't call setCurPage() because changing status to
+                // "success" moves events out of collection
 
-            // Force fetching top pending events
-            $eventCollection->clear();
-            // The loop doesn't call setCurPage() because changing status to
-            // "success" moves events out of collection
-
-            $events = [];
-            /** @var Event $event */
-            foreach ($eventCollection as $event) {
-                $events[] = $event->export();
-            }
-
-            if (!$events) {
-                $this->logger->debug("Fetched no pending events, stopping page loop");
-                break;
-            }
-
-            $this->logger->debug("Fetched events: " . count($events));
-            $this->logger->debug("Sending event page #{$pageNo} of {$eventCollection->getLastPageNumber()}");
-
-            $syncDate = $this->date->gmtDate();
-
-            $exception = null;
-            try {
-                $this->sendEvents($events);
-                $this->logger->debug("Event page sent OK");
-            } catch (Exception $exception) {
-                // Just assign $exception variable
-                $this->logger->error("Event page sending error: {$exception->getMessage()}");
-            }
-
-            $this->logger->debug("Updating processed event statuses");
-
-            $connection = $this->resource->getConnection();
-            $connection->beginTransaction();
-
-            /** @var Event $event */
-            foreach ($eventCollection as $event) {
-                if (is_null($exception)) {
-                    $event->setStatus('success');
-                    $event->setLastError(null);
-                    $event->setSyncedAt($syncDate);
-                } else {
-                    $event->setStatus('error');
-                    $event->setLastError($exception->getMessage());
+                $events = [];
+                /** @var Event $event */
+                foreach ($eventCollection as $event) {
+                    $events[] = $event->export();
                 }
 
+                if (!$events) {
+                    $this->logger->debug("Fetched no pending events, stopping page loop");
+                    break;
+                }
+
+                $this->logger->debug("Fetched events: " . count($events));
+                $this->logger->debug("Sending event page #{$pageNo} of {$eventCollection->getLastPageNumber()}");
+
+                $syncDate = $this->date->gmtDate();
+
+                $exception = null;
+                $notAcceptedEvents = [];
                 try {
-                    $this->eventRepository->save($event);
-                } catch (\Exception $e) {
-                    $this->logger->error("Error saving event #{$event->getId()}: {$e->getMessage()}");
-                    $connection->rollBack();
-                    throw new Exception($e->getMessage());
+                    $response = $this->sendEvents($events);
+                    if (isset($response['event_messages'])) {
+                        $notAcceptedEvents = $response['event_messages'];
+                    }
+                    $count = array(
+                        'accepted' => $response['n_valid_events'],
+                        'not-accepted' => count($notAcceptedEvents),
+                    );
+                    $this->logger->debug("Event page sent OK. Accepted events: {$count['accepted']}. Invalid events: {$count['not-accepted']}.");
+                } catch (Exception $exception) {
+                    // Just assign $exception variable
+                    $this->logger->error("Event page sending error: {$exception->getMessage()}");
+                }
+
+                $this->logger->debug("Updating processed event statuses");
+
+                $connection = $this->resource->getConnection();
+                $connection->beginTransaction();
+
+                /** @var Event $event */
+                foreach (array_values($eventCollection->getItems()) as $i => $event) {
+                    if (isset($notAcceptedEvents[$i])) {
+                        $event->setStatus('not-accepted');
+                        $event->setLastError($notAcceptedEvents[$i]);
+                        $event->setSyncedAt($syncDate);
+                    } elseif (is_null($exception)) {
+                        $event->setStatus('success');
+                        $event->setLastError(null);
+                        $event->setSyncedAt($syncDate);
+                    } else {
+                        $event->setStatus('error');
+                        $event->setLastError($exception->getMessage());
+                    }
+
+                    try {
+                        $this->eventRepository->save($event);
+                    } catch (\Exception $e) {
+                        $this->logger->error("Error saving event #{$event->getId()}: {$e->getMessage()}");
+                        $connection->rollBack();
+                        throw new Exception($e->getMessage());
+                    }
+                }
+
+                $connection->commit();
+
+                $this->logger->debug("Event statuses updated OK");
+
+                if (!is_null($exception)) {
+                    break;
                 }
             }
-
-            $connection->commit();
-
-            $this->logger->debug("Event statuses updated OK");
-
-            if (!is_null($exception)) {
-                break;
-            }
+        } else {
+            $this->logger->debug("No pending events found so just pinging API");
+            $this->sendPingEvent();
         }
 
         $this->logger->debug('Cleaning old synced events');
@@ -174,7 +188,29 @@ class SendEvents
     }
 
     /**
+     * Sends immediate ping event
+     * (consider a refactoring to move it out of cron task class as it's not
+     * really belong here)
+     */
+    public function ping()
+    {
+        try {
+            $this->initHttpClient();
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            return;
+        }
+
+        try {
+            $this->sendPingEvent();
+        } catch (Exception $e) {
+            $this->logger->critical($e);
+        }
+    }
+
+    /**
      * @param array $events
+     * @return array Decoded response
      * @throws Exception
      */
     protected function sendEvents(array $events)
@@ -182,8 +218,7 @@ class SendEvents
         $this->httpClient->setRawBody(json_encode(compact('events')));
 
         try {
-            $this->httpClient->send();
-            $response = $this->httpClient->getResponse();
+            $response = $this->httpClient->send();
             if (!$response->isOk()) {
                 throw new Exception("API error {$response->getStatusCode()}: {$response->getBody()}");
             }
@@ -191,6 +226,28 @@ class SendEvents
         catch (\Zend\Http\Exception\RuntimeException $e) {
             throw new Exception($e->getMessage());
         }
+
+        $response = json_decode($response->getBody(), true);
+        if (false === $response) {
+            throw new Exception("Error decoding JSON response: {$response->getBody()}");
+        }
+
+        /** @var array $response */
+        return $response;
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function sendPingEvent()
+    {
+        $this->sendEvents([
+            [
+                'event_type' => 'ping',
+                'event_details' => [],
+                'timestamp' => time(),
+            ],
+        ]);
     }
 
     /**
---
+++ Plugin/ConfigPlugin.php
@@ -0,0 +1,45 @@
+<?php
+namespace AristanderAi\Aai\Plugin;
+
+use AristanderAi\Aai\Helper\Data;
+use AristanderAi\Aai\Cron\SendEvents;
+use Magento\Config\Model\Config;
+
+class ConfigPlugin
+{
+    /** @var Data */
+    protected $helperData;
+
+    /** @var SendEvents */
+    protected $sendEvents;
+
+    public function __construct(
+        Data $helperData,
+        SendEvents $sendEvents
+    ) {
+        $this->helperData = $helperData;
+        $this->sendEvents = $sendEvents;
+    }
+
+    public function aroundSave(Config $subject, callable $proceed)
+    {
+        if ($this->helperData->getConfigPath() != $subject->getData('section')) {
+            // Not our section
+            return $proceed();
+        }
+
+        if ('' != $subject->getConfigDataValue('aai/general/api_key')) {
+            // Already configured
+            return $proceed();
+        }
+
+        $returnValue = $proceed();
+
+        if ('' != $subject->getConfigDataValue('aai/general/api_key')) {
+            // Change to non-empty
+            $this->sendEvents->ping();
+        }
+
+        return $returnValue;
+    }
+}
