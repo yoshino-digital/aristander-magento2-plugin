04f720666fd88b1292c4212a163f8b8a1c1fc506
Anton Oliinyk <ts2001@gmail.com>
2019-01-05 19:02:14 +0200

    add attribute on install/upgrade

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -8,6 +8,21 @@ use Magento\Framework\Setup\ModuleContextInterface;
 class Uninstall implements UninstallInterface
 {
     /**
+     * Eav setup factory
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function uninstall(
@@ -19,5 +34,13 @@ class Uninstall implements UninstallInterface
         $setup->getConnection()->dropTable($setup->getTable('aai_event'));
 
         $setup->endSetup();
+
+        /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create();
+
+        $eavSetup->removeAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'aai_backup_price'
+        );
     }
 }
---
+++ Setup/UpgradeData.php
@@ -0,0 +1,109 @@
+<?php
+namespace AristanderAi\Aai\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * Eav setup factory
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     * @param EavSetupFactory $eavSetupFactory
+     */
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    public function upgrade(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        if ('' == $context->getVersion()) {
+            $this->install($setup, $context);
+            return;
+        }
+
+        if (version_compare($context->getVersion(), '1.1', '<')) {
+            // Upgrade to v1.1
+            $this->addBackupPriceProductAttribute();
+        }
+    }
+
+    public function install(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $this->addBackupPriceProductAttribute();
+    }
+
+    public function addBackupPriceProductAttribute()
+    {
+        /** @var \Magento\Eav\Setup\EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create();
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'aai_backup_price',
+            [
+                'group' => 'General',
+                'type' => 'decimal',
+                'label' => 'Backup Price',
+                'input' => 'price',
+                'source' => '',
+                'frontend' => '',
+                'backend' => '',
+                'required' => false,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
+                'is_used_in_grid' => false,
+                'is_visible_in_grid' => false,
+                'is_filterable_in_grid' => false,
+                'visible' => false,
+                'is_html_allowed_on_front' => false,
+                'visible_on_front' => false,
+                
+                // TODO: remove if not needed
+                /*
+                'class' => '',
+                'user_defined' => true,
+                'default' => null,
+                'searchable' => false,
+                'filterable' => false,
+                'comparable' => false,
+                'used_in_product_listing' => false,
+                'unique' => false,
+                'apply_to' => array('simple', 'configurable', 'virtual', 'downloadable'),
+                'is_configurable' => false,
+                'is_used_for_promo_rules' => false,
+                */
+            ]
+        );
+
+        // TODO: remove if not needed
+        /*
+        // Hide from admin
+        $setup->updateAttribute(
+            'catalog_product',
+            'aai_backup_price',
+            'is_visible',
+            '0'
+        );
+        */
+    }
+}
