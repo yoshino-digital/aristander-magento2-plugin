2c2fd02f4c3ca034df086bd7151d4e99032df37b
Anton Oliinyk <ts2001@gmail.com>
2019-02-28 19:54:50 +0200

    Observer\ProductSave

--- Observer/ProductSave.php
+++ Observer/ProductSave.php
@@ -1,25 +1,32 @@
 <?php
 namespace AristanderAi\Aai\Observer;
 
+use AristanderAi\Aai\Helper\Price;
 use Magento\Catalog\Model\Product;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 
 class ProductSave implements ObserverInterface
 {
-    public $enabled = true;
+    /** @var Price */
+    private $helperPrice;
 
+    public function __construct(
+        Price $helperPrice
+    ) {
+        $this->helperPrice = $helperPrice;
+    }
+
+    /**
+     * @param Observer $observer
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
     public function execute(Observer $observer)
     {
-        // Do not trigger on import
-        if (!$this->enabled) {
-            return;
-        }
-
         /** @var Product $product */
         $product = $observer->getData('product');
-        if ($product->dataHasChangedFor('price')) {
-            $product->setData('aai_backup_price', null);
+        if ($product->dataHasChangedFor('tier_price')) {
+            $this->helperPrice->handleProductAlternativePriceUpdate($product);
         }
     }
 }
