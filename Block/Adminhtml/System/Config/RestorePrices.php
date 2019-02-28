de6013cdc1fe98bdb9a00d0571240f82f234e409
Anton Oliinyk <ts2001@gmail.com>
2019-02-28 14:52:15 +0200

    rewrite restore prices block

--- Block/Adminhtml/System/Config/RestorePrices.php
+++ Block/Adminhtml/System/Config/RestorePrices.php
@@ -6,42 +6,12 @@ use Magento\Backend\Block\Template\Context;
 
 class RestorePrices extends FullRow
 {
-    /** @var Data */
-    private $helperData;
-
-    public function __construct(
-        Context $context,
-        Data $helperData,
-        \Magento\Framework\UrlInterface $urlBuilder,
-        array $data = []
-    ) {
-        $this->helperData = $helperData;
-
-        parent::__construct($context, $data);
-    }
-
     /**
-     * Generates URL for given controller action
-     *
-     * @param string $action
-     * @return string
-     */
-    public function getActionUrl($action = 'index')
-    {
-        return $this->getUrl(
-            "aristander-ai/restorePrices/{$action}",
-            ['form_key' => $this->getFormKey()]
-        );
-    }
-
-    /**
-     * @inheritdoc Dynamically returns no-backup template for empty status
+     * @inheritdoc
      * @return string
      */
     public function getTemplate()
     {
-        return !empty($this->getStatus())
-            ? 'system/config/restore-prices.phtml'
-            : 'system/config/restore-prices/no-backup.phtml';
+        return 'system/config/restore-prices.phtml';
     }
 }
