20f1a72326ddea8a9583f2a70052fccedafeb418
Anton Oliinyk <ts2001@gmail.com>
2019-03-14 15:13:03 +0200

    copied M1 code

--- Service/EventRecorder/Order.php
+++ Service/EventRecorder/Order.php
@@ -144,4 +144,167 @@ class Order
 
         return $this;
     }
+
+    /**
+     * Requests shipping rate
+     *
+     * @param Mage_Sales_Model_Quote_Address $address
+     * @param $carrier
+     * @param $method
+     * @return float|null
+     */
+    private function requestShippingRate(
+        Mage_Sales_Model_Quote_Address $address,
+        $carrier,
+        $method
+    ) {
+        /**
+         * Based on core code
+         * @see \Mage_Sales_Model_Quote_Address::requestShippingRates
+         */
+
+        /** @var $request Mage_Shipping_Model_Rate_Request */
+        $request = Mage::getModel('shipping/rate_request');
+        $request->setAllItems($address->getAllItems());
+        $request->setDestCountryId($address->getCountryId());
+        $request->setDestRegionId($address->getRegionId());
+        $request->setDestRegionCode($address->getRegionCode());
+        /**
+         * need to call getStreet with -1
+         * to get data in string instead of array
+         */
+        $request->setDestStreet(
+            $address->getStreet(
+                Mage_Sales_Model_Quote_Address::DEFAULT_DEST_STREET
+            )
+        );
+        $request->setDestCity($address->getCity());
+        $request->setDestPostcode($address->getPostcode());
+        $request->setPackageValue($address->getBaseSubtotal());
+        $packageValueWithDiscount = $address->getBaseSubtotalWithDiscount();
+        $request->setPackageValueWithDiscount($packageValueWithDiscount);
+        $request->setPackageWeight($address->getWeight());
+        $request->setPackageQty($address->getItemQty());
+
+        /**
+         * Need for shipping methods that use insurance based on price of physical products
+         */
+        /** @noinspection PhpUndefinedMethodInspection */
+        $packagePhysicalValue = $address->getBaseSubtotal() - $address->getBaseVirtualAmount();
+        $request->setPackagePhysicalValue($packagePhysicalValue);
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $request->setFreeMethodWeight($address->getFreeMethodWeight());
+
+        /**
+         * Store and website identifiers need specify from quote
+         */
+        /*$request->setStoreId(Mage::app()->getStore()->getId());
+        $request->setWebsiteId(Mage::app()->getStore()->getWebsiteId());*/
+
+        $request->setStoreId($this->store->getId());
+        $request->setWebsiteId($this->store->getWebsiteId());
+        $request->setFreeShipping($address->getFreeShipping());
+        /**
+         * Currencies need to convert in free shipping
+         */
+        $request->setBaseCurrency($this->store->getBaseCurrency());
+        $request->setPackageCurrency($this->store->getCurrentCurrency());
+        $request->setLimitCarrier($carrier);
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $request->setBaseSubtotalInclTax(
+            $address->getBaseSubtotalInclTax() + $address->getBaseExtraTaxAmount()
+        );
+
+        /** @noinspection PhpUndefinedMethodInspection */
+        $rateResult = Mage::getModel('shipping/shipping')
+            ->collectRates($request)
+            ->getResult();
+
+        if (!$rateResult) {
+            // Request failed
+            return null;
+        }
+
+        /** @var Mage_Shipping_Model_Rate_Result_Method $rate */
+        /** @noinspection PhpUndefinedMethodInspection */
+        foreach ($rateResult->getAllRates() as $rate) {
+            if ($method == $rate->getData('method')) {
+                return $rate->getData('price');
+            }
+        }
+
+        // Method not found
+        return null;
+    }
+
+    /**
+     * Prepares to original shipping rate request
+     *
+     * @param Mage_Sales_Model_Quote $quote
+     * @throws ReflectionException
+     */
+    private function beforeRequestShippingRates(Mage_Sales_Model_Quote $quote)
+    {
+        $this->store = $quote->getStore();
+
+        $reflectionClass = new ReflectionClass($this->store);
+        $this->configCacheReflection = $reflectionClass->getProperty(
+            '_configCache'
+        );
+        $this->configCacheReflection->setAccessible(true);
+        $this->configCacheBackup = $this->configCacheReflection->getValue(
+            $this->store
+        );
+        if (null === $this->configCacheBackup) {
+            $this->configCacheBackup = array();
+        }
+    }
+
+    /**
+     * Restores config after original shipping rate request
+     */
+    private function afterRequestShippingRates()
+    {
+        if (null !== $this->configCacheBackup) {
+            $this->configCacheReflection->setValue(
+                $this->store,
+                $this->configCacheBackup
+            );
+        }
+
+        $this->configCacheReflection->setAccessible(false);
+        $this->configCacheReflection = null;
+        $this->configCacheBackup = null;
+        $this->store = null;
+    }
+
+    /**
+     * Sets temporary shipping carries config value
+     *
+     * @param string $carrierCode
+     * @param string $field
+     * @param string $value
+     */
+    private function setTmpCarrierConfig($carrierCode, $field, $value)
+    {
+        $this->setTmpStoreConfig(
+            "carriers/{$carrierCode}/{$field}",
+            $value
+        );
+    }
+
+    /**
+     * Sets temporary store config value via cache config hack
+     *
+     * @param string $path
+     * @param string $value
+     */
+    private function setTmpStoreConfig($path, $value)
+    {
+        $cache = $this->configCacheReflection->getValue($this->store);
+        $cache[$path] = $value;
+        $this->configCacheReflection->setValue($this->store, $cache);
+    }
 }
