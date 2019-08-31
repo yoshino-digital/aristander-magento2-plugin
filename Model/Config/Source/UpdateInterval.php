586808e3d3e54a625d8b4657287bb49302a6bee1
Anton Oliinyk <ts2001@gmail.com>
2019-08-31 19:27:21 +0300

    Add Update Interval field

---
+++ Model/Config/Source/UpdateInterval.php
@@ -0,0 +1,28 @@
+<?php
+namespace AristanderAi\Aai\Model\Config\Source;
+
+class UpdateInterval implements \Magento\Framework\Option\ArrayInterface
+{
+    /**
+     * Options getter
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => '30',
+                'label' => __('30 minutes'),
+            ],
+            [
+                'value' => '60',
+                'label' => __('1 hour'),
+            ],
+            [
+                'value' => '120',
+                'label' => __('2 hours'),
+            ],
+        ];
+    }
+}
